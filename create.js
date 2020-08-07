683521efce71e61982aa013e902cd25ba7e5b004
Tomas <tomas.milkintas1@gmail.com>
2020-08-07 22:45:23 +0100

    added crud for serverless

--- create.js
+++ create.js
@@ -1,21 +1,19 @@
 import * as uuid from "uuid";
-import AWS from "aws-sdk";
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
 
-export function main(event, context, callback) {
-    // Request body is passed in as a JSON encoded string in 'event.body'
+export const main = handler(async (event, context) => {
     const data = JSON.parse(event.body);
     const params = {
         TableName: process.env.tableName,
-        // 'Item' contains the attributes of the item to becreated
-        // - 'userId': user identities are federatedthrough the
-        //Cognito Identity Pool, we will usethe identity id
-        //as the user id of the authenticated user
+        // 'Item' contains the attributes of the item to be created
+        // - 'userId': user identities are federated through the
+        //             Cognito Identity Pool, we will use the identity id
+        //             as the user id of the authenticated user
         // - 'noteId': a unique uuid
         // - 'content': parsed from request body
         // - 'attachment': parsed from request body
         // - 'createdAt': current Unix timestamp
-
         Item: {
             userId: event.requestContext.identity.cognitoIdentityId,
             noteId: uuid.v1(),
@@ -25,30 +23,7 @@ export function main(event, context, callback) {
         },
     };
 
-    dynamoDb.put(params, (error, data) => {
-        // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-        const headers = {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true,
-        };
+    await dynamoDb.put(params);
 
-        // Return status code 500 on error
-        if (error) {
-            const response = {
-                statusCode: 500,
-                headers: headers,
-                body: JSON.stringify({ status: false }),
-            };
-            callback(null, response);
-            return;
-        }
-
-        // Return status code 200 and the newly created item
-        const response = {
-            statusCode: 200,
-            headers: headers,
-            body: JSON.stringify(params.Item),
-        };
-        callback(null, response);
-    });
-}
+    return params.Item;
+});
